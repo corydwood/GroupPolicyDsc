095e685dc6c9522c8b113fde949830fa4a87e63b
Cory Wood <corydwood@gmail.com>
2016-02-23 11:21:24 -0600

    Created xGroupPolicy Module.

---
+++ Tests/Integration/MSFT_xGPInheritance.config.ps1
@@ -0,0 +1,16 @@
+﻿$ou = 'OU=Test OU,DC=testdomain,DC=local'
+$GPInheritance = @{
+    Target = $ou
+    Server = 'localhost'
+    Ensure = 'Present'
+}
+configuration 'MSFT_xGPInheritance_config' {
+    Import-DscResource -Name 'MSFT_xGPInheritance'
+    node localhost {
+       xGPInheritance Integration_Test {
+            Target = $GPInheritance.Target
+            Server = $GPInheritance.Server
+            Ensure = $GPInheritance.Ensure
+       }
+    }
+}
